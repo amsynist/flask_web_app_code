ccaa0444f159ea2aafbdefa44ea06cae55e6944a
amsynist <pravinblogging@gmail.com>
2021-04-27 00:05:54 +0530

    nwew

--- market/__init__.py
+++ market/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
@@ -12,5 +11,4 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
-
 from market import routes
